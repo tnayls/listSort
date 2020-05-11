b3ae004730f4b853d46837f674388a67d3da5625
tnayls <you@email.comtom.n@live.com.a>
2020-05-11 14:22:16 +1000

    2

--- script.js
+++ script.js
@@ -4,26 +4,55 @@ async function loadData(url) {
     return data;
   }
   
+  //sorting the lsit
+  function sortlist( list ) {
+    list.sort( (item1, item2) => {
+      if(item2.name > item1.name ) {
+        return -1
+      }
+      else {
+        return 1;
+      }
+    } )
+  }
+  
+  //rendering the masterlist
+  function rednderMaster( list, element ) {
+    list.forEach( (item) => {
+      let listItem = `<li>
+      ${item.name}
+      <div class="toolbar">
+      <button type="button"
+      data-action="perishable"
+      data-id="${item.id}"
+      data-name="${item.name}"
+      data-unit="${item.unit}"
+      data-category="${item.category}"
+      data-quantity="${item.quantity}">
+      Perishable
+      </button>
+      <button type="button">Non Perishable</button>
+      </div>
+      </li>`;
+      element.insertAdjacentHTML('beforeend', listItem);
+    })
+  }
+  
   let masterList = new Array();
+  let perishablesList = [];
   window.addEventListener('load', () => {
     // load the data
     const dataFile = 'data.json';
-    // let response = await fetch(dataFile);
-    // if( response.ok ) {
-    //   let data = await response.json();
-    //   console.log( response );
-    // }
-    // second try
+    
+    
+    
     loadData( dataFile )
     .then( (data) => { 
       const masterDisplay = document.querySelector('#master');
       data.forEach( (item) => {
-        let id = item.id;
-        let name = item.name;
-        masterList.push( item );
-        let listItem = `<li data-id="${id}">${name}</li>`;
-        masterDisplay.insertAdjacentHTML('beforeend', listItem );
+       masterList.push( item );
       })
+    sortlist( masterList);
+    rednderMaster( masterList, masterDisplay )
     })
-  
   })
